98288d2daf9cceb8913f73aec1813b32aed52907
guillermaster <guillermaster@gmail.com>
2017-07-26 12:08:56 -0500

    initial commit

---
+++ DualSliderBar/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DualSliderBar.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["Title"] = "Short Terms Loans Online";
+            return View();
+        }
+        
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
